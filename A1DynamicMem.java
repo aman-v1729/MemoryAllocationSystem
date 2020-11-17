cc8f43018db7301345963a1216e17b6ead0f29d8
Aman Verma <amanverma1729@gmail.com>
2020-11-17 21:02:12 +0530

    Completed A1DynamicMem.java

--- A1DynamicMem.java
+++ A1DynamicMem.java
@@ -26,10 +26,31 @@ public class A1DynamicMem extends DynamicMem {
     // (A1List.java).
 
     public int Allocate(int blockSize) {
+        if (blockSize < 1)
+            return -1;
+        Dictionary block = freeBlk.Find(blockSize, false);
+        if (block != null) {
+            if (block.size > blockSize) {
+                freeBlk.Insert(block.address + blockSize, block.size - blockSize, block.size - blockSize);
+            }
+            allocBlk.Insert(block.address, blockSize, block.address);
+            int blockAdd = block.address;
+            freeBlk.Delete(block);
+            return blockAdd;
+        }
         return -1;
     }
 
     public int Free(int startAddr) {
+        if (startAddr < 0)
+            return -1;
+        Dictionary block = allocBlk.Find(startAddr, true);
+        if (block != null) {
+            int blockSize = block.size;
+            freeBlk.Insert(block.address, block.size, block.size);
+            allocBlk.Delete(block);
+            return 0;
+        }
         return -1;
     }
 }
